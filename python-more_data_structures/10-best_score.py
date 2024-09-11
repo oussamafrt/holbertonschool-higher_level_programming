6ef593551e339a2084a371106d8cb3719ce2cdc5
oussamafrt <oussama.ferhat01@gmail.com>
2024-09-11 11:48:42 +0200

    Write a function that returns a key with the biggest integer value.

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,13 +7,3 @@ def best_score(a_dictionary):
         if value > a_dictionary[best_key]:
             best_key = key
     return best_key
-
-#!/usr/bin/python3
-best_score = __import__('10-best_score').best_score
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
