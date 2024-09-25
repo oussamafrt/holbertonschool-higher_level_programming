e76d46da491919bb5e56e0e908cabcfa2be8c533
oussamafrt <oussama.ferhat01@gmail.com>
2024-09-25 21:43:10 +0200

    Write a class Rectangle that inherits from BaseGeometry

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Shebang line indicating the interpreter for the script."""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a new instance"""
+    def __init__(self, width, height):
+        """Initializes the rectangle"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[rectangle] {}/{}".format(self.__width, self.__height))
