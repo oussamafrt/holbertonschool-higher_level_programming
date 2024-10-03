e7c2d0115892d6961d0765ab09781d5c6ebadd0e
oussamafrt <oussama.ferhat01@gmail.com>
2024-10-03 22:25:07 +0200

    Write a function that writes a string to a text file (UTF8) and returns the number of characters written:

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """Function that read a text file"""
     with open(filename, "w", encoding="UTF8",) as file:
-        content = file.read()
-        print(content, end="")
+        return(file.write(text))
