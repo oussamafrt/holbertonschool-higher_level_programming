dff64c9a362b1631b557ab00cea1e33185a2bcdc
oussamafrt <oussama.ferhat01@gmail.com>
2024-10-03 22:33:14 +0200

    Write a function that returns the JSON representation of an object (string):

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,6 +2,9 @@
 """Shebang line indicating the interpreter for the script."""
 
 
+import json
+
+
 def to_json_string(my_obj):
     """Function that return a text file"""
     return to_json_string(my_obj)
