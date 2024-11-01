9e24460c7b1e65af53ef21dea49a75cc7e1e274d
oussamafrt <oussama.ferhat01@gmail.com>
2024-11-01 13:27:22 +0100

    initial commit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,32 @@
-#!/usr/bin/oythin3
-
-import MySQLdb
+#!/usr/bin/python3
+"""
+This script lists all states from
+the database hbtn_0e_0_usa whith a name starting with N.
+"""
 import sys
+import MySQLdb
 
-def main():
-    """The main entry point of the script."""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == "__main__":
+    """
+    The main entry point of the script.
+    Retrieves MySQL username, password, and database name from
+    command line arguments and calls list_states function.
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
 
     db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
+        host='localhost',
+        port=3306, user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
     cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-
-    results = cursor.fetchall()
-
-    for row in results:
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
 
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    main()
