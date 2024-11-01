3192a5afa713ee0ddde54f5aa9ca6302a2023189
oussamafrt <oussama.ferhat01@gmail.com>
2024-11-01 13:59:58 +0100

    initial commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-"""shebang script"""
+"""Definition of the State class and Base instance for SQLAlchemy ORM."""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 import sys
-from model_state import Base, State
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that links to the MySQL table states."""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
