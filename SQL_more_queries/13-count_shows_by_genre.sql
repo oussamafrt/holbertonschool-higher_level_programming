6c88f14f522ad00969a7391ce4aeba5feb1c1cf6
oussamafrt <oussama.ferhat01@gmail.com>
2024-11-01 12:13:54 +0100

    initial commit

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,14 +1,14 @@
 -- Select genres and the count of shows linked to each genre
 SELECT 
-    genres.name AS genre, 
-    COUNT(tv_show_genres.show_id) AS number_of_shows
+    tg.name AS genre, 
+    COUNT(tsg.show_id) AS number_of_shows
 FROM 
-    genres
-LEFT JOIN 
-    tv_show_genres ON genres.id = tv_show_genres.genre_id
+    tv_genres AS tg
+JOIN 
+    tv_show_genres AS tsg ON tg.id = tsg.genre_id
 GROUP BY 
-    genres.id
+    tg.name
 HAVING 
-    COUNT(tv_show_genres.show_id) > 0
+    COUNT(tsg.show_id) > 0
 ORDER BY 
     number_of_shows DESC;
